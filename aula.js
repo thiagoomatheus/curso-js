26e38f2a3ad683e58ea3a22feabcb4d851b73fae
Thiago Oliveira Matheus <117959734+thiagoomatheus@users.noreply.github.com>
2023-01-26 21:25:26 -0300

    aula 1

---
+++ aula.js
@@ -0,0 +1,16 @@
+//Criando Varíaveis
+
+//let idade = 5;
+//console.log(idade);
+//let altura = 171;
+//console.log(altura);
+//let nome = 'Thiago';
+//console.log(nome);
+
+//Criando obejtos
+
+let usuario = {
+    email: 'asdalaksl@gmail.com',
+    senha: 123456
+}
+console.log(usuario);
