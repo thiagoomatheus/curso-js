bbcb476d4aa03f521d1a87b37d58d25c52b49499
Thiago Oliveira Matheus <117959734+thiagoomatheus@users.noreply.github.com>
2023-02-09 20:48:01 -0300

    aula 12

--- aula.js
+++ aula.js
@@ -519,3 +519,75 @@ let dia2 = data2.getDay();
 console.log(dia2); // Retorna o dia da semana = 6
 
 */
+
+/* -----------------
+
+// Mini Projeto 11 - Montador de endereço
+
+let endereco = 'Digite seu endereço';
+
+console.log(endereco);
+
+function MontadorEndereco(rua,numero,cidade,cep) {
+    this.rua = rua,
+    this.numero = numero,
+    this.cidade = cidade,
+    this.cep = cep
+};
+
+endereco = new MontadorEndereco('Benedito de Olivera Melo',28,'Paranapanema',18720000);
+
+function exibirEndereco(endereco) {
+    for (chave in endereco) {
+    console.log(chave,endereco[chave])
+    }
+}
+
+exibirEndereco(endereco);
+
+*/
+
+/* ----------------------
+
+Muito válido esse projeto. Aprendi a comparar os valores de dois objetos. O prefoessr fez de outra forma que também serve para situação de validação.
+
+// Mini-Projeto 12 - Encontre a igualdade de Objetos
+
+function MontadorEndereco(rua,numero,cidade,cep) {
+    this.rua = rua,
+    this.numero = numero,
+    this.cidade = cidade,
+    this.cep = cep
+}
+
+function exibirEndereco(endereco) {
+    for (chave in endereco) {
+    console.log(chave,endereco[chave])
+    }
+}
+
+function comparaEndereco(obj1,obj2) {
+    let objeto1 = obj1;
+    let objeto2 = obj2;
+
+    for (chave in objeto2) {
+        if (objeto1[chave] === objeto2[chave]) {
+            console.log(`${chave} é igual`);
+        }
+        else {
+            console.log(`${chave} é diferente`);
+        }
+    }
+
+}
+
+let endereco1 = new MontadorEndereco('Benedito de Olivera Melo',28,'Paranapanema',18720000);
+exibirEndereco(endereco1);
+
+let endereco2 = new MontadorEndereco('Jose Venâncio Aires',11,'Paranapanema',18720000)
+exibirEndereco(endereco2)
+
+comparaEndereco(endereco1,endereco2)
+
+*/
+
