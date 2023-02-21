eae191a47965bcf198caa0c3ca179909cf456cbb
Thiago Oliveira Matheus <117959734+thiagoomatheus@users.noreply.github.com>
2023-02-21 18:41:51 -0300

    aula 17

--- exercicios/mini-projeto15/aula.js
+++ exercicios/mini-projeto15/aula.js
@@ -1,16 +1,20 @@
 const anfitrioes = ['Rafael','Angela','Maria','João'];
 
-let nomeUsuario = 'Insira seu nome';
-let inputNomeUsuario = document.getElementById("nomeusuario");
-inputNomeUsuario.value = nomeUsuario;
+let convidado = 'Digite seu nome ...';
+
+function recebeNomeUsuario(nomeUsuario) {
+    if (nomeUsuario !== '') {
+        convidado = nomeUsuario;
+    }
+}
 
 function validaConvidado(nomeAnfitriao) {
     if (anfitrioes.includes(nomeAnfitriao)) {
-        document.getElementById("resultado-validacao").innerText = `${nomeUsuario} - Você pode entrar!`;
+        document.getElementById("resultado-validacao").innerText = `${convidado} - Você pode entrar!`;
         document.getElementById("resultado-validacao").style.color = 'green';
     }
     else {
-        document.getElementById("resultado-validacao").innerText = `${nomeUsuario} - Você não pode entrar!`;
+        document.getElementById("resultado-validacao").innerText = `${convidado} - Você não pode entrar!`;
         document.getElementById("resultado-validacao").style.color = 'red';
     }
 }
