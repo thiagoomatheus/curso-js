b0768ab41fff951bb86b83f50d36c6eb03d9476d
Thiago Oliveira Matheus <117959734+thiagoomatheus@users.noreply.github.com>
2023-04-06 21:47:33 -0300

    projeto3 - 0.2

--- exercicios/projeto3/js/scripts.js
+++ exercicios/projeto3/js/scripts.js
@@ -5,12 +5,16 @@ const loadingElement = document.querySelector("#loading");
 const postsConatiner = document.querySelector("#posts-container");
 
 const postPage = document.querySelector("#post")
-const postContainer = document.querySelector("#posts-container")
-const commentsContainer = document.querySelector("#comments-container")
+const postContainer = document.querySelector("#post-container")
+const commentContainer = document.querySelector("#comment-container")
+
+const emailArea = document.querySelector("#email")
+const commentArea = document.querySelector("#comment")
+const submitComment = document.querySelector("#submit-comment")
 
 const urlSearch = new URLSearchParams(window.location.search);
 
-const postId = urlSearch.get("id");
+const postId = urlSearch.get('id');
 
 async function getAllPosts() {
     const resposnse = await fetch(url);
@@ -29,7 +33,7 @@ async function getAllPosts() {
         title.textContent = post.title;
         body.textContent = post.body;
         link.textContent = "Ler";
-        link.setAttribute("href", `/exercicios/projeto3/post.html?id=?${post.id}`)
+        link.setAttribute("href", `/exercicios/projeto3/post.html?id=${post.id}`)
 
         div.appendChild(title);
         div.appendChild(body);
@@ -44,7 +48,7 @@ async function getPost(id) {
     const [resposnsePost, resposnseComment] = await Promise.all ([
         fetch(`${url}/${id}`),
         fetch(`${url}/${id}/comments`)
-    ])
+    ]);
 
     const dataPost = await resposnsePost.json();
 
@@ -52,8 +56,64 @@ async function getPost(id) {
 
     loadingElement.classList.add("hide");
 
-    postPage.classList.remove("hide")
+    postPage.classList.remove("hide");
 
+    const title = document.createElement("h1");
+
+    const body = document.createElement("p");
+
+    title.innerText = dataPost.title;
+    body.innerText = dataPost.body;
+
+    postContainer.appendChild(title);
+    postContainer.appendChild(body);
+
+    dataComment.map((comment) => {
+        createComment(comment)
+    })
+
+    submitComment.addEventListener("click", function(e) {
+
+        e.preventDefault()
+        
+        let userEmail = emailArea.value;
+        let userComment = commentArea.value;
+
+        newComment(userEmail, userComment)
+    })
+}
+
+function createComment(comment) {
+    const div = document.createElement("div");
+    const name = document.createElement("h3");
+    const commentBody = document.createElement("p");
+
+    name.innerText = comment.email;
+    commentBody
+    .innerText = comment.body;
+    
+    div.appendChild(name);
+    div.appendChild(commentBody);
+
+    commentContainer.appendChild(div)
+}
+
+async function newComment(userEmail, userComment) {
+    let comment = {
+        email: userEmail,
+        body: userComment
+    }
+    const resposnse = await fetch(`${url}/${postId}/comments`, {
+        method: "POST",
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify(comment)     
+    })
+
+    let data = await resposnse.json()
+
+    createComment(data)
 }
 
 if (!postId) {
@@ -61,4 +121,14 @@ if (!postId) {
 }
 else {
     getPost(postId)
+
+    // submitComment.addEventListener("submit", function(e) {
+
+    //     e.preventDefault()
+        
+    //     let userEmail = emailArea.value;
+    //     let userComment = commentArea.value;
+
+    //     newComment(userEmail, userComment)
+    // })
 }
